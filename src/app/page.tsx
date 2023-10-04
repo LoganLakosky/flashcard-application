173e5eab2b0a8e0b79424b1e05a531c5b0f95911
Logan <59356357+LoganLakosky@users.noreply.github.com>
2023-10-03 19:57:04 -0500

    flash card app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
 export default function Home() {
-  return <main className="mainPageContainer"></main>;
+  return (
+    <main className="mainPageContainer">
+      <div className="flashCardMainContainer">
+        <div className="flashCardMainTop">
+          <h1>Flash card Collection</h1>
+        </div>
+        <div className="flashCardsMainContainer">
+          <div className="flashCardsTopContainer">
+            <div className="box1"></div>
+            <div className="box1"></div>
+            <div className="box1"></div>
+          </div>
+          <div className="flashCardsBottomContainer">
+            <div className="box1"></div>
+            <div className="box1"></div>
+            <div className="box1"></div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
